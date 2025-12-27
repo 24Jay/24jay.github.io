4979fb78d514c4f253a0068097a70d607b145405
quantux <24zhangjie@gmail.com>
2025-12-27 12:24:34 +0800

    quick commit

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766027398',resources: [
+    cacheName: 'chirpy-1766809456',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
