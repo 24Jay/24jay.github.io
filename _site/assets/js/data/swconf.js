2f2284cf020582513cb41ee2b040de004f3fdc10
quantux <24zhangjie@gmail.com>
2025-12-28 16:43:10 +0800

    gae

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1766910342',resources: [
+    cacheName: 'chirpy-1766911370',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
