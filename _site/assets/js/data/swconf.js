f0f8f685318bbdf6bc0918b3f8e15fd6a1d3edf8
quantux <24zhangjie@gmail.com>
2026-02-01 23:08:08 +0800

    dapo

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1769958431',resources: [
+    cacheName: 'chirpy-1769958477',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
